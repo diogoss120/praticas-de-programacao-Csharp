490f12ec52ff522536aa07c8e5d5e35ad2b1f5fb
Diogo Souza Santos <ddiiogo.souza25710@hotmail.com>
2020-08-02 13:22:22 -0300

    Add files via upload

---
+++ POO/LambdaComLinq/ExercicioLambdComLinq/Entities/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioLambdComLinq
+{
+   public class Employee
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public double Salaly { get; set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            Nome = name;
+            Email = email;
+            Salaly = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome}, {Email} - {Salaly}";
+        }
+    }
+}
---
+++ POO/LambdaComLinq/ExercicioLambdComLinq/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace ExercicioLambdComLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            using (StreamReader reader = File.OpenText(@"D:\exercicio.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string[] funcionario = reader.ReadLine().Split(',');
+                    string nome = funcionario[0];
+                    string email = funcionario[1];
+                    double salario = double.Parse(funcionario[2], CultureInfo.InvariantCulture);
+                    employees.Add(new Employee(nome, email, salario));
+                }
+            }
+
+            ExibirEmployee(employees, "Lista completa de nomes, emails e salarios ");
+
+            var emails = employees.Where(e => e.Salaly > 3000).OrderBy(e => e.Email).Select(e => e.Email);
+            var nomes = employees.Where(e => e.Nome[0] == 'A').Sum(e => e.Salaly);
+
+            ExibirEmployee(emails, "Email das pessoas que ganham mais de 3000 reais");
+            Console.WriteLine("Soma do salario de todas as pessoas em que a primeira letra do nome é a A: "
+                + nomes);
+            Console.WriteLine();
+        }
+        public static void ExibirEmployee<T>(IEnumerable<T> emp, string msg)
+        {
+            Console.WriteLine(msg);
+            foreach (var e in emp)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine();
+        }
+    }
+}
